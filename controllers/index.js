1c91cf7c3d77f3d07999abf13be80ab27056e411
Troy Johnson <tnj8510@gmail.com>
2023-02-11 01:15:13 -0800

    adding dashboard

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
+const landing = require("./landing")
+const dashboard = require("./dashboard")
 const api = require('./api');
 
+router.use("/", landing)
+router.use("/dashboard", dashboard)
 router.use("/api", api)
 
 module.exports = router;
---
+++ controllers/landing.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const { Captured, Location, Player, Prototype, Wild } = require("../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const wildArr = await Wild.findAll();
+    // const captured = capturedArr.map((captured) => captured.get({ plain: true }));
+
+    res.render('landing', wildArr);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router
