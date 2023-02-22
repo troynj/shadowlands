0373099c5ffd34fea02f9b29c482472fe340ebb1
troynj <troynjohnson6@gmail.com>
2023-02-21 17:59:44 -0800

    updating about

---
+++ controllers/about.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+
+const { Arena, Captured, Journey, Player, Prototype, ShadowBeast, User, Wild } = require("../models");
+
+router.get("/", async (req, res) => {
+  try {
+    res.render('about');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router
--- controllers/index.js
+++ controllers/index.js
@@ -6,12 +6,13 @@ const journey = require("./journey")
 const selectMonster = require("./selectMonster")
 const api = require('./api');
 const auth = require('./auth');
+const about = require('./about')
 
 router.use("/", landing)
 router.use("/Dashboard", dashboard)
 router.use("/SelectMonster", selectMonster)
 router.use("/Journey", journey)
-router.use("/About", dashboard)
+router.use("/About", about)
 router.use("/api", api)
 router.use("/Auth", auth)
 
